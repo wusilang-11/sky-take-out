c42982100dd0278c13cdb1499acda5dc92eb290b
Lenovo <wusilang123@gmail.com>
2025-10-24 23:13:33 +0800

    工作台业务模块功能代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -88,4 +89,13 @@ public interface DishMapper {
      * @return
      */
     int updateStatus(Dish updateDish);
+
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
+
